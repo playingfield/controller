5d83f87dec76a26075285e1a5e7613220c588539
Bas <bas.meijer@me.com>
2024-11-05 15:41:45 +0100

    Update ansible.sh
    Signed-off-by: Bas <bas.meijer@me.com>

--- ansible.sh
+++ ansible.sh
@@ -4,4 +4,3 @@ python3 -m venv ~/py3
 source ~/py3/bin/activate
 pip install --upgrade pip wheel
 pip install -r requirements.txt
-ansible-galaxy collection install -r ansible/collections/requirements.yml
