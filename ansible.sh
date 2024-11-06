1d827400b836dcabec77a0a7ced588b3daeb92ae
Bas Meijer <bas.meijer@me.com>
2024-11-06 20:40:06 +0100

    Requirements for RHEL/Alma/Rocky 8

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,7 @@
 # Just `source ./ansible.rc`
+deactivate
 rm -rf ~/.py3
-python3 -m venv ~/py3
+python3.12 -m venv ~/py3
 source ~/py3/bin/activate
 pip install --upgrade pip wheel
 pip install -r requirements.txt
